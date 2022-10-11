fd7fd1f7b98798cfbdeff4184aabfe5dfd305fa6
Umole Andrew <39883080+umole@users.noreply.github.com>
2022-10-11 03:53:30 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.common.exceptions import StaleElementReferenceException
 import time
 
 
@@ -10,28 +11,22 @@ driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 driver.get("http://orteil.dashnet.org/experiments/cookie/")
 cookie = driver.find_element(By.ID, "cookie")
 
-
-available_lifelines = driver.find_elements(By.CSS_SELECTOR, "#store div")
-life_line = driver.find_elements(By.CSS_SELECTOR, "#store div b")
-life_prices = []
-try:
-    for life in life_line:
-        life_price = life.text.split("-")[1]
-        life_prices.append(int(life_price.strip(" ").replace(",", "")))
-
-except IndexError:
-    pass
-
-
 id_of_div = [element.get_attribute("id") for element in driver.find_elements(By.CSS_SELECTOR, "#store div")]
-print(id_of_div)
 
 time_on = True
 time_out = time.time() + 5
-while time.time() < time.time() + 60*5:
+while time_on:
     try:
         cookie.click()
         if time.time() > time_out:
+            life_line = driver.find_elements(By.CSS_SELECTOR, "#store div b")
+            life_prices = []
+            try:
+                for life in life_line:
+                    life_price = life.text.split("-")[1]
+                    life_prices.append(int(life_price.strip(" ").replace(",", "")))
+            except IndexError:
+                pass
             coupon = int(driver.find_element(By.ID, "money").text.replace(",", ""))
             lives_less_than_coupon = [x for x in life_prices if x < coupon]
             print(lives_less_than_coupon)
@@ -40,9 +35,15 @@ while time.time() < time.time() + 60*5:
             else:
                 price_index = life_prices.index(max(lives_less_than_coupon))
                 print(price_index)
+                available_lifelines = driver.find_elements(By.CSS_SELECTOR, "#store div")
                 available_lifelines[price_index].click()
             time_out += 5
-    except:
+        if time.time() > time.time() + 60*5:
+            time_on = False
+            click_per_sec = driver.find_element(By.XPATH, '//*[@id="cps"]').text.split(":")[1]
+            print(click_per_sec.strip(" "))
+
+    except StaleElementReferenceException as e:
         pass
 
 
